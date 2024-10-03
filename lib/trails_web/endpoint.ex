da725a078c9b1f35789cbc79f1088d05e5762b87
David Lange <lange@finiam.com>
2024-10-03 19:26:45 +0100

    add socket

--- lib/trails_web/endpoint.ex
+++ lib/trails_web/endpoint.ex
@@ -1,4 +1,5 @@
 defmodule TrailsWeb.Endpoint do
+  alias TrailsWeb.UserSocket
   use Phoenix.Endpoint, otp_app: :trails
 
   # The session will be stored in the cookie and signed,
@@ -14,6 +15,10 @@ defmodule TrailsWeb.Endpoint do
   socket "/live", Phoenix.LiveView.Socket,
     websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
+
+  socket "/socket", UserSocket,
+    websocket: true,
+    longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
   #
